ce9c3d27e5025a7305833dbc50bddec4efc16018
trung.nguyen <trungab2305@gmail.com>
2025-02-16 10:26:01 +0700

    Update Set Up a Google Cloud Network

--- Professional Cloud Architect Certification/Set Up a Google Cloud Network/run.sh
+++ Professional Cloud Architect Certification/Set Up a Google Cloud Network/run.sh
@@ -1,3 +1,29 @@
+# Input REGION_1
+if [[ -z "$REGION_1" ]]; then
+    read -p "Input REGION_1: " REGION_1
+fi
+
+# Input REGION_2
+if [[ -z "$REGION_2" ]]; then
+    read -p "Input REGION_2: " REGION_2
+fi
+
+# Input REGION_3
+if [[ -z "$REGION_3" ]]; then
+    read -p "Input REGION_3: " REGION_3
+fi
+
+# Export env
+export INPUT_ERROR="${RED}Errors: Please setup REGION_1, REGION_2, REGION_3 env before run script.${RESET}"
+
+# Check env
+if [[ -z "$REGION_1" || -z "$REGION_2" || -z "$REGION_3"]]; then
+    echo -e $INPUT_ERROR
+    exit 1
+fi
+
+export REGION_1 REGION_2 REGION_3
+
 gcloud config set compute/zone "us-east4-a"
 export ZONE=$(gcloud config get compute/zone)
 
@@ -6,11 +32,11 @@ export REGION=$(gcloud config get compute/region)
 
 gcloud compute networks create taw-custom-network --subnet-mode custom
 
-gcloud compute networks subnets create subnet-us-east4 --network taw-custom-network --region us-east4 --range 10.0.0.0/16
+gcloud compute networks subnets create subnet-$REGION_1 --network taw-custom-network --region $REGION_1 --range 10.0.0.0/16
 
-gcloud compute networks subnets create subnet-europe-west1 --network taw-custom-network --region europe-west1 --range 10.1.0.0/16
+gcloud compute networks subnets create subnet-$REGION_2 --network taw-custom-network --region $REGION_2 --range 10.1.0.0/16
 
-gcloud compute networks subnets create subnet-us-east1 --network taw-custom-network --region us-east1 --range 10.2.0.0/16
+gcloud compute networks subnets create subnet-$REGION_3 --network taw-custom-network --region $REGION_3 --range 10.2.0.0/16
 
 gcloud compute networks subnets list --network taw-custom-network
 
